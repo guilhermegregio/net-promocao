fb9c70a141a97c462794dbbeb600586f6c10f463
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2014-07-29 10:00:42 -0300

    Adicionado mongo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Types.ObjectId;
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Adperformance' });
+mongoose.connect('mongodb://localhost/netPromocao');
+
+var adSchema = mongoose.Schema({
+}, { strict: false });
+
+var Ad = mongoose.model('Ad', adSchema);
+
+var cepSchema = mongoose.Schema({
+}, { strict: false });
+
+var Cep = mongoose.model('Cep', cepSchema);
+
+router.post('/', function (req, res) {
+	var anuncio = new Ad(req.body);
+
+	//	anuncio.save();
+
+	if (anuncio.get('cliente-net') === 'sim') {
+		res.redirect('/conversao-cliente.html');
+	} else {
+		res.redirect('/cadastro-final.html');
+	}
+});
+
+router.post('/cadastro', function (req, res) {
+	var anuncio = new Ad(req.body);
+
+	//	anuncio.save();
+
+	var atende = Cep.findOne({CEP: Number(anuncio.get('cep'))}, 'CEP CIDADE', function (err, cep) {
+		if (err) res.json(err);
+		if (cep !== null) {
+			res.redirect('/conversao-nao-cliente.html');
+		} else {
+			res.redirect('/nao-atende.html');
+		}
+	});
+
 });
 
 module.exports = router;
