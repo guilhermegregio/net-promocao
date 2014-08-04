24c75421a36a8ec062d816c21926feedb1c6e907
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2014-08-04 10:13:39 -0300

    redirects

--- routes/index.js
+++ routes/index.js
@@ -27,9 +27,9 @@ router.post('/', function (req, res) {
 		anuncio.save();
 
 		if (anuncio.get('cliente-net') === 'sim') {
-			res.redirect('/conversao-cliente.html');
+			res.redirect('/net-promocao/conversao-cliente.html');
 		} else {
-			res.redirect('/cadastro-final.html');
+			res.redirect('/net-promocao/cadastro-final.html');
 		}
 	});
 });
@@ -39,9 +39,9 @@ router.post('/cadastro', function (req, res) {
 
 	Ad.findOneAndUpdate({_id: req.cookies.anuncio._id}, req.body, function (err, place) {
 		if (anuncio.get('cepAtende')) {
-			res.redirect('/conversao-nao-cliente.html');
+			res.redirect('/net-promocao/conversao-nao-cliente.html');
 		} else {
-			res.redirect('/nao-atende.html');
+			res.redirect('/net-promocao/nao-atende.html');
 		}
 	});
 
